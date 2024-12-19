d336f6acfed6ec187b853d8fb6f23bc8f7ced089
Nuridin <nurmannuridin28@gmail.com>
2024-12-19 13:54:14 +0500

    add descr to project

--- src/api/projects.js
+++ src/api/projects.js
@@ -2,21 +2,16 @@ import todo from "../assets/todo.png";
 export const projects = [
   {
     title: "ToDo App",
-    descr: "Some description",
+    descr:
+      "A simple and intuitive to-do list app designed to help you organize tasks, set deadlines, and track progress. With easy task creation, categorization, and prioritization features, it ensures you stay focused and productive throughout your day.",
     img: todo,
     github: "https://github.com/Nuridin28/ToDo",
     url: "https://nuridin28.github.io/ToDo/",
   },
   {
     title: "Portfolio App",
-    descr: "Some description",
-    img: todo,
-    github: "https://github.com/Nuridin28/ToDo",
-    url: "https://nuridin28.github.io/ToDo/",
-  },
-  {
-    title: "ToDo App",
-    descr: "Some description",
+    descr:
+      "A sleek and user-friendly to-do app designed to help users efficiently manage tasks, set deadlines, and track progress. Features include task creation, categorization, prioritization, and easy-to-use interface, making it an essential tool for staying organized and productive. Ideal for individuals seeking to enhance their task management experience.",
     img: todo,
     github: "https://github.com/Nuridin28/ToDo",
     url: "https://nuridin28.github.io/ToDo/",
